37e14b8ed791f484d8dc028fb556728c31a35b54
Kodi Arfer <git@arfer.net>
2015-07-21 20:05:51 -0400

    Checked in the run.sh for the POSIX bundle.

---
+++ tools/posix-run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$TERM" = screen ] ; then
+    TERM=screen-256color
+else
+    TERM=xterm-256color
+fi
+
+PYTHON_CMD=python
+V=$("$PYTHON_CMD" -c 'import sys; print(sys.version_info[0])')
+if [ "$V" = 3 ] ; then
+    # "python" is Python 3. Better hope that Python 2 is
+    # available at "python2".
+    PYTHON_CMD=python2
+fi
+
+cd lib
+"$PYTHON_CMD" run.pyc "$@"
