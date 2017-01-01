5ae9b397b38c1d7ca1254b7590458d1402753020
Kodi Arfer <git@arfer.net>
2017-01-01 13:45:56 -0800

    Updated bundle run scripts for Python 3.

--- tools/run-windows.sh
+++ tools/run-windows.sh
@@ -6,8 +6,7 @@ rm -f "$TEMP"/rply-*.json
   # seem to offer much of a speedup.
 
 TERM=xterm-256color
-PYTHON_CMD=python2.7
 export LANG=en_US.UTF-8
 export ROGUETV_BUNDLE_INFO="$(cat VERSION)"
 cd lib
-exec "$PYTHON_CMD" run.pyc "$@"
+exec python3 run.pyc "$@"
